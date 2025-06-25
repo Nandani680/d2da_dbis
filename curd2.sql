aa98fedc51cbdda5b7c9e186771ee295561074c0
Nandani680 <shahnandini549@gmail.com>
2025-06-25 15:59:42 +0530

    Add files via upload

---
+++ curd2.sql
@@ -0,0 +1,20 @@
+-- studentcreate database d2dA;
+
+-- create table Student(id int,st_name text,age int);
+-- insert into student(id,st_name,age) values(1,'sumit',23);
+-- select*from student;
+ -- insert into student(id,st_name,age) values 
+-- (2,'amit',35),
+ -- (3,'pradip',55),
+ -- (4,'mark',34);
+--  insert into student(id,st_name)values
+ -- (11,'sumit'),
+ -- (22,'bhavin');
+ use d2dA;
+
+--  update student set st_name='pratik' where id=1;
+	-- update student set id = 100 where st_name = 'pratik';
+    -- delete from student where id =100;
+    delete from student where id = 34;
+    delete from student where st_name ='bhavin';
+ 
